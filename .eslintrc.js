bd7289940e664cdfcab749539fadf9438e4ff52c
nganb2014674 <nganb2014674@student.ctu.edu.vn>
2024-11-28 21:06:29 +0700

    Cai dat cac handler truy xuat CSDL

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
-	env: {
-		node: true,
-		commonjs: true,
-		es2021: true,
-	},
-	extends: ['eslint:recommended','prettier'],
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'prettier'],
+    rules: {
+        'no-multi-spaces': 'error', // Cấm khoảng trắng thừa
+        'space-before-function-paren': ['error', 'never'], // Không có khoảng trắng trước dấu ( trong hàm
+        'no-unused-vars': 'warn', // Cảnh báo biến không được sử dụng
+    },
 };
